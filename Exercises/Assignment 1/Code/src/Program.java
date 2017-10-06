d95ad7bcf0bef0a719846f5b70acc8d25abb4732
Anders Wind <aws@netcompany.com>
2017-10-06 08:37:25 +0200

    Imported all the code files and imported the progal library

--- Exercises/Assignment 1/Code/src/Program.java
+++ Exercises/Assignment 1/Code/src/Program.java
@@ -1,6 +1,20 @@
-public class Program {
-    
-    public static void main(String[] args){
+import TSP.*;
 
+public class Program {
+
+    public static void main(String[] args){
+        solveGraph(new Instance1());
+        solveGraph(new Instance2());
+        solveGraph(new Instance3());
+    }
+
+    public static void solveGraph(Graph g){
+        BranchAndBound_TSP solver = new BranchAndBound_TSP(g);
+        long start = System.nanoTime();
+        BnBNode n = solver.solve();
+        long end = System.nanoTime();
+        System.out.printf("Took %.2fms\n",(end-start)/1000000.0);
+		System.out.println(n);
+		Visualization.visualizeSolution(g, n);//Requires ProGAL (www.diku.dk/~rfonseca/ProGAL)
     }
 }
